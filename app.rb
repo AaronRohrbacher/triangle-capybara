f7fff34221f2c2047bb3b0061ebb99943e98adac
Aaron Rohrbacher and Sara Spink <sara@>
2017-09-12 08:37:16 -0700

    triangle tracker with capybara tests

--- app.rb
+++ app.rb
@@ -8,10 +8,11 @@ get ('/') do
   erb(:input)
 end
 
-get ('/output') do
+post ('/output') do
   side1 = params.fetch("side1").to_i
   side2 = params.fetch("side2").to_i
   side3 = params.fetch("side3").to_i
   triangle = Triangle.new(side1, side2, side3)
   @triangle_out = triangle.is_triangle
+  erb(:output)
 end
---
+++ spec/triangles_integration_spec.rb
@@ -0,0 +1,16 @@
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('the triangle path', {:type => :feature}) do
+  it('processes user entry and returns type of triangle') do
+    visit('/')
+    fill_in('side1', :with => '5')
+    fill_in('side2', :with => '5')
+    fill_in('side3', :with => '5')
+    click_button('Submit')
+
+    expect(page).to have_content('equilateral')
+  end
+end
